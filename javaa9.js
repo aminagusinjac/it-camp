a44ee8e1ca59468a5d5fdf637e69a2e2444a53ff
unknown <a.gusinac@hotmail.com>
2022-06-01 18:21:49 +0200

    font

---
+++ javaa9.js
@@ -0,0 +1,42 @@
+//ZADATAK 1
+
+// const miles = [15, 20, 1, 60, 45, 120]
+
+// const prviZadatak = (array) => {
+
+// const milesToKilometers = array.map ((el)=> el * 1.60 );
+
+// const totalDistanceInKilometers=milesToKilometers.reduce((prev, curr) => prev + curr)
+
+// return totalDistanceInKilometers;
+// }
+
+// console.log(prviZadatak(miles))
+
+
+//ZADATAK 2
+
+//SABRATI KVADRATE A ONDA SUMIRATI map vraca drugi niz , filter ne menja niz , samo ga pretrazujr 
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const drugiZadatak=(array)=>
+// {
+//    const parniNiz= array.filter((el)=> el%2===0)
+
+//    const kvadratParnih = parniNiz.map((el) => el * el)
+
+//    const suma=kvadratParnih.reduce((prev, curr) => prev + curr )
+// return suma;
+
+// } 
+
+// console.log(drugiZadatak(numbers))
+
+//treci zadatak\
+
+let strings = ["avengers", "captain america", "ironman", "black panther"]
+
+const toUppercase = (array) => {
+    const newString = array.map => (el)  
+}
--- vezbanjee1.js
+++ vezbanjee1.js
@@ -48,17 +48,66 @@
 //     if ()
 // }
  //EVERY
-const array1 = [1, 2, 3];
+                // const array1 = [1, 2, 3];
 
-console.log(array1.includes(2));
-//dal niz koji pretrazuje sadrzi dvojku i on ce da vraca bolean; 
+                // console.log(array1.includes(2));
+                // //dal niz koji pretrazuje sadrzi dvojku i on ce da vraca bolean; 
 
-const pets = ['cat', 'dog', ' bat'];
+                // const pets = ['cat', 'dog', ' bat'];
 
-console.log(pets.includes('dog'))
+                // console.log(pets.includes('dog'))
 
-//TRY IT 
+                // //TRY IT 
 
-const elements = ["Fire", "Air", "Water", "trece"];
+                // const elements = ["Fire", "Air", "Water", "trece"];
 
-console.log(elements.join(''))
+                //     console.log(elements.join(''))
+
+
+//ZADATAK SA 7 VEZBI
+// const cars = ["BMW", "Audi", "Smart"]
+
+
+// cars.push ("Mercedes") //dodaje na zadnje mesto
+// cars.unshift("pasat") //dodaje na prvo mesto
+// cars.reverse(cars) //okrene niz
+// cars.shift(cars) //brise prvi cas
+// cars.pop(cars) //brise poslednji  clan 
+// console.log(cars)
+
+
+
+//ZADATAK 8 
+
+//NAPISI FUNKCIJU KOJA IZ NIZA VRACA BROJEVE VECE ILI JEDNAKO OD 18
+
+// const niz = [12, 24, 56, 78, 98, 38, 9]
+
+// function punoletni(ages) {
+//     let niz1=[];
+//     for(let i of ages) {
+//         if(i>=18) {
+//             niz1.push(i)
+//         }
+
+//     }
+//     return niz1;
+// }
+
+// console.log(punoletni(niz));  
+
+const niz1=[1,68, 9,9,0,54,76,67]
+const niz2=["audi", "mercedes", "smart", "golf"]
+let niz3 = []
+niz3  = niz1.concat(niz2) //za spajanje 2 niza 
+
+
+console.log(niz3);
+
+
+
+
+
+
+
+
