c80f21a17d5e54826870a377be4be57e844e073d
unknown <a.gusinac@hotmail.com>
2022-06-21 23:12:06 +0200

    font

---
+++ objekat4.js
@@ -0,0 +1,52 @@
+res  = [1, 2, 3, 4, 5].map((el) => {  //map iste duzine koristi se kad treba nesto da uradimo nad svakim elementom
+    if (el >= 3) {
+        return el;
+    }
+});
+
+res2 = [1, 2, 3, 4, 5].filter((el)  =>{
+    
+})   //koristi se kad treba da iskoristimo svaki element 
+
+// console.log(1);
+// console.log(2);
+// func = () => {
+//     console.log(3);
+// };
+
+// eetTimeopt(func, 1000)
+//https://www.jsv9000.app
+//VISULEAR
+
+promise//objekat
+
+posao = new Promise ((resolve, reject) => {
+    response = 400;
+
+for(let i= 0; i < 90000; i ++) {}
+    if(response ===200){
+        resolve();
+    } else{
+        reject();
+    }
+    });
+
+    if (response === 200) {
+        resolve ();
+        else {
+            reject();
+        }
+    });
+
+
+posao  
+.then (data) => {
+    console.log("uspesno povukao podatke", data);
+    arr = data.filter((el) => el %2 === 0);
+    return arr;
+
+})
+
+.then((arg1)) => {
+    
+}
